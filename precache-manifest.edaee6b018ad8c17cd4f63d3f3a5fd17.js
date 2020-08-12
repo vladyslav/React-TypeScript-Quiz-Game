09685541842cc801902aedd0c553cb75e247aa2d
Vladyslav Vasylenko <vladyslav.vasylenko@dev.pro>
2020-08-12 22:17:01 +0300

    Updates

---
+++ precache-manifest.edaee6b018ad8c17cd4f63d3f3a5fd17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b246fa8262257e3f56fd53c15fe3763",
+    "url": "/React-TypeScript-Quiz-Game/index.html"
+  },
+  {
+    "revision": "a6a393f844e553dd1ef7",
+    "url": "/React-TypeScript-Quiz-Game/static/js/2.dfe5f9b6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-TypeScript-Quiz-Game/static/js/2.dfe5f9b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c5742910bdb159c228",
+    "url": "/React-TypeScript-Quiz-Game/static/js/main.90d07710.chunk.js"
+  },
+  {
+    "revision": "b14f2b4a962c97fa5935",
+    "url": "/React-TypeScript-Quiz-Game/static/js/runtime-main.4f7f9c18.js"
+  },
+  {
+    "revision": "c11f97a3b321dad2ede69652d3f8a174",
+    "url": "/React-TypeScript-Quiz-Game/static/media/nattu-adnan.c11f97a3.jpg"
+  }
+]);
